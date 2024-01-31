ea7ece5ae7affd40bab492152f308584435289c0
joaolucassolanoufv <joaolucassilvasolano@gmail.com>
2024-01-31 15:36:44 -0300

    updating unit tests to music model

--- review_app/tests/unit_tests/models/test_music.py
+++ review_app/tests/unit_tests/models/test_music.py
@@ -23,8 +23,9 @@ class MusicModelTest(TestCase):
         artist_name = 'The Beatles'
         artist = create_artist(artist_name)
         music = create_music(music_name, [artist])
-        self.assertEqual(music.artists.count(), 1)
-        self.assertEqual(music.artists.all()[0], artist)
+        expected_music_artists = [artist]
+        music_artists = list(music.artists.all())
+        self.assertEqual(music_artists, expected_music_artists)
 
     def test_music_with_more_than_one_artits(self):
         music_name = 'Get Lucky'
@@ -33,9 +34,9 @@ class MusicModelTest(TestCase):
         artist1 = create_artist(artist1_name)
         artist2 = create_artist(artist2_name)
         music = create_music(music_name, [artist1, artist2])
-        self.assertEqual(music.artists.count(), 2)
-        self.assertEqual(music.artists.all()[0], artist1)
-        self.assertEqual(music.artists.all()[1], artist2)
+        expected_music_artists = [artist1, artist2]
+        music_artists = list(music.artists.all())
+        self.assertEqual(music_artists, expected_music_artists)
 
     def test_music_to_string_with_one_artist(self):
         music_name = 'Something'
